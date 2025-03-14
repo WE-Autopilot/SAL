c99e0e764524c36e11f6d9962b9a06b50660c334
Hamza Elkababji <hamza.kababji@gmail.com>
2025-03-14 13:28:55 -0400

    Reverted some of the stuff and started new implementation of training mode

--- src/controller.py
+++ src/controller.py
@@ -10,9 +10,6 @@ from waypoint_manager import WaypointManager
 
 class Controller(AbstractController):
 
-    def __init__(self):
-        self.startup()
-
     def startup(self):
         
         ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
